e7b900d3d82a1cf00ae283a037dbbe18d9f0745d
adambrgmn <adam@wordpressutvecklare.se>
2019-09-23 13:16:53 +0200

    Changing cookie naming to match the automatic tests

--- model/Cookie.php
+++ model/Cookie.php
@@ -3,24 +3,24 @@
 namespace login\model;
 
 class Cookie {
-
-  public function setCookie (string $username, string $password) {
-			setcookie("username", $username, time() + 1800);
-			setcookie("password", $password, time() + 1800);
+  
+  public function setCookie ($username, $password) {
+			setcookie("LoginView::CookieName", $username, time() + 1800);
+      setcookie("LoginView::CookiePassword", $password, time() + 1800);
   }
 
   public function removeCookie () {
-    setcookie("username", "", time() - 1800);
-    setcookie("password", "", time() - 1800);
+    setcookie("LoginView::CookieName", "", time() - 1800);
+    setcookie("LoginView::CookiePassword", "", time() - 1800);
   }
 
   public function hasCookie () : bool {
-    return isset($_COOKIE["username"]) && isset($_COOKIE["password"]);
+    return isset($_COOKIE["LoginView::CookieName"]) && isset($_COOKIE["LoginView::CookiePassword"]);
   }
 
   public function getUserCredentialsByCookie () : \login\model\UserCredentials {
-    $username = new \login\model\Username($_COOKIE["username"]);
-    $pass = new \login\model\Password($_COOKIE["password"]);
+    $username = new \login\model\Username($_COOKIE["LoginView::CookieName"]);
+    $pass = new \login\model\Password($_COOKIE["LoginView::CookiePassword"]);
 
     return new \login\model\UserCredentials($username, $pass, true);
   }
