3171646cdae72bd89b13bff0b3bde6645c82af3d
adambrgmn <adam@wordpressutvecklare.se>
2019-10-11 10:33:18 +0200

    Commenting back

--- application/model/API.php
+++ application/model/API.php
@@ -19,36 +19,36 @@ class API {
         }
     }
 
-    // public function fetchJobs (string $keyword, string $city) {
-    //     $phrase = $keyword;
-    //     $phrase .= '%20';
-    //     $phrase .= $city;
+    public function fetchJobs (string $keyword, string $city) {
+        $phrase = $keyword;
+        $phrase .= '%20';
+        $phrase .= $city;
 
-    //     $url = 'https://jobsearch.api.jobtechdev.se/search?q=' . $phrase . '&offset=0&limit=2';
+        $url = 'https://jobsearch.api.jobtechdev.se/search?q=' . $phrase . '&offset=0&limit=2';
 
-    //     $ch = curl_init();
+        $ch = curl_init();
 
-    //     curl_setopt($ch, CURLOPT_URL, $url);
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 
-    //     $filterResult = 'X-Fields: hits{headline, description, number_of_vacancies, application_deadline, 
-    //         logo_url, application_details{url}, workplace_address{municipality}, employment_type{label}, employer{name, url}}';
+        $filterResult = 'X-Fields: hits{headline, description, number_of_vacancies, application_deadline, 
+            logo_url, application_details{url}, workplace_address{municipality}, employment_type{label}, employer{name, url}}';
 
-    //     curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
-    //         'Accept: application/json',
-    //         'api-key: ' . $this->api,
-    //         $filterResult
-    //         ));
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
+            'Accept: application/json',
+            'api-key: ' . $this->api,
+            $filterResult
+            ));
 
-    //     $data = curl_exec($ch);
-    //     curl_close($ch);
+        $data = curl_exec($ch);
+        curl_close($ch);
 
-    //     $json = json_decode($data, true);
-    //     return $json;
-    // }
+        $json = json_decode($data, true);
+        return $json;
+    }
 
-    // public function convertJobs (Array $jobs) {
+    public function convertJobs (Array $jobs) {
         
-    // }
+    }
 }
